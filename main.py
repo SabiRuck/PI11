0d0c66eebf254369e49e37e0b668800b80adcfc2
SabiRuck <146921408+SabiRuck@users.noreply.github.com>
2023-10-25 10:56:27 +0200

    Update main.py-delete idk, sucit and sucet together

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ a = int(input("Zadaj cislo a :"))
 b = int(input("Zadaj cislo b :"))
 
 print("hello world")
-print("Sucet je", a+b)
-print("Sucin je", a*b)
-#idk
+print("Sucin je", a*b,"a sucet je",a+b)
+
 
 
 
