b77e1ca727a3fd7857d7c9bd02e514cd9e7d87a7
Sabi Ruckova <sabinka.ruckova@gmail.com>
2023-11-08 09:38:30 +0100

    copy everything  from school

---
+++ 01-uvod/main.py
@@ -0,0 +1,12 @@
+print("Hello World!")  #vypisuje text do konzoly
+
+name = input("What's your name?: ") #input = ulozit zadany udaj do premennej (name)
+year = input("In which year you were born:")
+print("Hello " + name + ".")
+
+age = 2023 - int(year)  #int ()= premena textu na cisla
+print("You are ", str(age), "years old")  #str() = premena cisla na vetu/slovo   # mozem pouzivat (x , x) anebo (x + x)
+
+
+
+
