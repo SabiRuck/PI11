3fcc480cd642af680288273515067a5f779fd006
Sabi Ruckova <sabinka.ruckova@gmail.com>
2023-11-08 09:11:18 +0100

    Add math_random.py

---
+++ math_random.py
@@ -0,0 +1,23 @@
+#import  math as matematika o=2 * matematika.pi * r
+#import math  o=2 * math.pi * r
+#from math import * o = 2 * p i *r
+from math import pi
+import random
+
+r = 10
+o = 2  * pi * r
+print (round(o,2)) #round = zaokruhli cislo na dany pocet miest
+
+number = random.randrange(0,10)
+number2 = random.randint(10,20) #2. moznost ta 1. sa poziva viac
+color = random.choice(("red", "green", "blue"))
+print (number)
+print (number2)
+print(color)
+
+
+
+
+
+
+
