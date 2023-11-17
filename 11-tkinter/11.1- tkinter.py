a411e20de21f04410908ff7db460e7998749a7ee
Sabi <sabinka.ruckova@gmail.com>
2023-11-17 17:33:58 +0100

    color in homework

--- 11-tkinter/11.1- tkinter.py
+++ 11-tkinter/11.1- tkinter.py
@@ -1,28 +1,34 @@
 import tkinter as tk
 import random
 
-canvas = tk.Canvas(width=600, height=500)
-canvas.pack()
-
 x = 10
 y = 10
 d = 50
 w = 1
-count = 600 // d    # " // " = celociselne delenie 7 // 3 = 2
-count2 = 500 // d
+times = 7
 
+width = d * times + x
+height = (d + d // 2) * times + y
+count = width // d    # " // " = celociselne delenie 7 // 3 = 2
+count2 = height // d
 stvrtina = d // 4
 
-canvas.create_rectangle(x,  y+d // 2, x+d, y+(d // 2)+d)
-canvas.create_polygon(x, y+d//2, x+2*stvrtina, y, x+d, y+d//2, outline="black", fill="#f0f0ed")
-canvas.create_rectangle(x+stvrtina, y+d // 2+stvrtina, x+3*stvrtina, y+d // 2+3*stvrtina)
-canvas.create_line(x+stvrtina, y+d // 2+2*stvrtina, x+3*stvrtina,y+d // 2+2*stvrtina)
-canvas.create_line(x+2*stvrtina, y+d // 2+stvrtina, x+2*stvrtina, y+d // 2+3*stvrtina)
+
+canvas = tk.Canvas(width=width, height=height)
+canvas.pack()
 
 
 
-
-
+for i in range(count2):
+    for j in range(count):
+        canvas.create_rectangle(x,  y+d // 2, x+d, y+(d // 2)+d)
+        canvas.create_polygon(x, y+d//2, x+2*stvrtina, y, x+d, y+d//2, outline="black", fill="#f0f0ed")
+        canvas.create_rectangle(x+stvrtina, y+d // 2+stvrtina, x+3*stvrtina, y+d // 2+3*stvrtina)
+        canvas.create_line(x+stvrtina, y+d // 2+2*stvrtina, x+3*stvrtina,y+d // 2+2*stvrtina)
+        canvas.create_line(x+2*stvrtina, y+d // 2+stvrtina, x+2*stvrtina, y+d // 2+3*stvrtina)
+        x = x + d
+    y = y + d + d // 2
+    x = x - d*count
 
 
 canvas.mainloop()
--- 11-tkinter/11.1-tkinter_house.py
+++ 11-tkinter/11.1-tkinter_house.py
@@ -1,16 +1,21 @@
 import tkinter as tk
 import random
 
-canvas = tk.Canvas(width=600, height=500)
-canvas.pack()
-
 x = 10
 y = 10
 d = 30
 w = 1
-"""
-count = 600 // d    # " // " = celociselne delenie 7 // 3 = 2
-count2 = 500 // d
+times = 25
+
+width = d * times + x
+height = d * times + y
+count = width // d    # " // " = celociselne delenie 7 // 3 = 2
+count2 = height // d
+
+
+canvas = tk.Canvas(width=width, height=height)
+canvas.pack()
+
 
 for j in range(count2):
     for i in range(count):
@@ -22,15 +27,5 @@ for j in range(count2):
         x = x + d
     y = y + d
     x = x - d * count
-"""
-pocet = 598 // d    # // je celociselne delenie 7 // 3 = 2
-for j in range(498 // d):
-    for i in range(pocet):
-        color = random.choice(("green", "red", "blue", "orange", "purple"))
-        canvas.create_rectangle(x, y, x+d, y+d, fill = color)
-        canvas.create_line(x, y, x+d, y+d)
-        canvas.create_line(x, y+d, x+d, y)
-        x = x + d  #nakresli stvorcek vedla dalsieho stvorceka
-    y = y + d
-    x = x-d*pocet
+
 canvas.mainloop()
